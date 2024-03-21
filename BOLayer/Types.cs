442dc28be9ae0761d63964b480999f289f37648f
Mary Welcey Guillen <welceyg2gmail.com>
2024-03-21 18:51:37 -0300

    Add initial files
    Add BOLayer for all business logic reqs
    Add Poker project file for UI related

---
+++ BOLayer/Types.cs
@@ -0,0 +1,26 @@
+﻿namespace BOLayer
+{
+    public enum Suit
+    {
+        Hearts,
+        Diamonds,
+        Clubs,
+        Spades
+    }
+
+    public enum FaceValue
+    {
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Jack,
+        Queen,
+        King,
+        Ace
+    }
+}
